4b86f6220a3ec7e4912770bf7264d6e15af7a471
Darlan Belfort <darlanbelfortofcl@gmail.com>
2026-08-17 20:39:36 -0300

    Modificações

--- src/AulaDevDojo/Aula001OlaDevDojo.java
+++ src/AulaDevDojo/Aula001OlaDevDojo.java
@@ -1,14 +1,8 @@
 package AulaDevDojo;
 
-public class Main{
+public class Aula001OlaDevDojo {
     public static void main(String[]args) {
-
-        String nome = "Darlan Oliveira Ferreira Belfort";
-        String endereco = "Povoado Formosa do Rio Corda";
-        String dataRecebimentoSalario = "17/08/2026";
-        double salario = 2551.59;
-        String relatorio = "Eu, "+nome+"\nmorando no endereço "+endereco+"\nconfirmo que recebi o salário de "+salario+" reais"+"\nna data de "+dataRecebimentoSalario;
-
-        System.out.println(relatorio);
+        //Impressão do Olá Dev Dojo
+        System.out.println("Olá Dev Dojo");
     }
 }
--- src/AulaDevDojo/Aula005OperadoresLogicosAnd.java
+++ src/AulaDevDojo/Aula005OperadoresLogicosAnd.java
@@ -1,6 +1,6 @@
 package AulaDevDojo;
 
-public class Aula005LogicosAnd {
+public class Aula005OperadoresLogicosAnd {
     public static void main(String[] args) {
 
     }
